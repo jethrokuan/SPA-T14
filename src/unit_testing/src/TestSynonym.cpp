f80b5c2700b87283f5572b0568fcf0f3d2d59824
Sriram Sami <frizensami@gmail.com>
2019-01-30 18:20:30 +0800

    Add QuoteIdent and tests

--- src/unit_testing/src/TestSynonym.cpp
+++ src/unit_testing/src/TestSynonym.cpp
@@ -2,47 +2,92 @@
 
 #include "query_evaluator/pql.h"
 
-TEST_CASE("Empty Declaration") {
+TEST_CASE("Empty Synonym") {
   auto d = QE::Synonym::construct("");
   REQUIRE(!d);
 }
 
-TEST_CASE("Single letter declaration") {
+TEST_CASE("Single letter Synonym") {
   auto d = QE::Synonym::construct("p");
   REQUIRE(d);
 }
 
-TEST_CASE("Single number declaration") {
+TEST_CASE("Single number Synonym") {
   auto d = QE::Synonym::construct("1");
   REQUIRE(!d);
 }
 
-TEST_CASE("Single space declaration") {
+TEST_CASE("Single space Synonym") {
   auto d = QE::Synonym::construct(" ");
   REQUIRE(!d);
 }
 
-TEST_CASE("Single underscore declaration") {
+TEST_CASE("Single underscore Synonym") {
   auto d = QE::Synonym::construct("_");
   REQUIRE(!d);
 }
 
-TEST_CASE("Multi-letter declaration") {
+TEST_CASE("Multi-letter Synonym") {
   auto d = QE::Synonym::construct("pn");
   REQUIRE(d);
 }
 
-TEST_CASE("Multi-letter with trailing number declaration") {
+TEST_CASE("Multi-letter with trailing number Synonym") {
   auto d = QE::Synonym::construct("pn1");
   REQUIRE(d);
 }
 
-TEST_CASE("Interleaving letter and number declaration") {
+TEST_CASE("Interleaving letter and number Synonym") {
   auto d = QE::Synonym::construct("pn1as");
   REQUIRE(d);
 }
 
-TEST_CASE("Single special character declaration") {
+TEST_CASE("Single special character Synonym") {
   auto d = QE::Synonym::construct("@");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Empty Synonym with quotes") {
+  auto d = QE::Synonym::construct("");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Single letter Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"p\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Single number Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"1\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Single space Synonym with quotes") {
+  auto d = QE::Synonym::construct("\" \"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Single underscore Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"_\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Multi-letter Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"pn\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Multi-letter with trailing number Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"pn1\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Interleaving letter and number Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"pn1as\"");
+  REQUIRE(!d);
+}
+
+TEST_CASE("Single special character Synonym with quotes") {
+  auto d = QE::Synonym::construct("\"@\"");
   REQUIRE(!d);
 }

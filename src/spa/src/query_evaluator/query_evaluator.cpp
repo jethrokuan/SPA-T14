61c16994ec8cd38e5eb70458ec07a1e3a645934a
Sriram Sami <frizensami@gmail.com>
2019-01-31 19:24:07 +0800

    Add stub code for query process until PKB call

--- src/spa/src/query_evaluator/query_evaluator.cpp
+++ src/spa/src/query_evaluator/query_evaluator.cpp
@@ -1,10 +1,14 @@
 #include "query_evaluator/query_evaluator.h"
 #include <iostream>
+#include "query_evaluator/core/query_preprocessor.h"
 
 using namespace QE;
 
 std::unique_ptr<QueryResult> QueryEvaluator::makePqlQuery(
     std::string& pql_query_string) {
   std::cout << pql_query_string << std::endl;
+  auto qp = QueryPreprocessor();
+  auto query = qp.getQuery(pql_query_string);
+  // The query needs to be fed into the PKB to get the QueryResult
   return std::unique_ptr<QueryResult>(nullptr);
 }

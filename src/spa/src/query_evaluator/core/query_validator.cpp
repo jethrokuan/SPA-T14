835597fed21ee0f381a24b3ef5d2473eb27fa5e6
Sriram Sami <frizensami@gmail.com>
2019-02-15 18:57:20 +0800

    Add validation for such_that clauses using synonyms that are not declared

--- src/spa/src/query_evaluator/core/query_validator.cpp
+++ src/spa/src/query_evaluator/core/query_validator.cpp
@@ -8,6 +8,7 @@ using namespace QE;
 void QueryValidator::validateQuery(const Query& query) {
   validatePatternVariableAsAssign(query);
   validateModifyUsesNoFirstArgUnderscore(query);
+  validateSuchThatSynonyms(query);
 }
 
 void QueryValidator::validatePatternVariableAsAssign(const Query& query) {
@@ -55,4 +56,52 @@ void QueryValidator::validateModifyUsesNoFirstArgUnderscore(
         }
       },
       such_that_firstarg);
+}
+
+void QueryValidator::validateSuchThatSynonyms(const Query& query) {
+  if (query.such_that == nullptr) {
+    return;
+  }
+  // Error if first arg is underscore otherwise
+  auto such_that_firstarg = query.such_that->getFirstArg();
+  auto such_that_secondarg = query.such_that->getSecondArg();
+
+  // Lack of code reuse here to allow for more expressive error messages
+  // Can be changed in the future: TODO
+  std::visit(
+      [&](auto&& arg) {
+        if (std::holds_alternative<Synonym>(arg)) {
+          auto found_declaration =
+              std::find_if(query.declarations->begin(),
+                           query.declarations->end(), [&](auto decl) {
+                             return decl.getSynonym().synonym ==
+                                    std::get<Synonym>(arg).synonym;
+                           });
+          if (found_declaration == query.declarations->end()) {
+            throw PQLValidationException(
+                "Cannot find a matching declaration for synonym " +
+                std::get<Synonym>(arg).synonym + " in first argument of " +
+                getStringFromRelation(query.such_that->getRelation()));
+          }
+        }
+      },
+      such_that_firstarg);
+  std::visit(
+      [&](auto&& arg) {
+        if (std::holds_alternative<Synonym>(arg)) {
+          auto found_declaration =
+              std::find_if(query.declarations->begin(),
+                           query.declarations->end(), [&](auto decl) {
+                             return decl.getSynonym().synonym ==
+                                    std::get<Synonym>(arg).synonym;
+                           });
+          if (found_declaration == query.declarations->end()) {
+            throw PQLValidationException(
+                "Cannot find a matching declaration for synonym " +
+                std::get<Synonym>(arg).synonym + " in second argument of " +
+                getStringFromRelation(query.such_that->getRelation()));
+          }
+        }
+      },
+      such_that_secondarg);
 }

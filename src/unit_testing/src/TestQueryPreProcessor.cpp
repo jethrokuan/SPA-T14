221a54530558478a3c0e9d3657b7b93edb171964
jiholim <jiholim1303@gmail.com>
2019-02-18 17:12:50 +0800

    Add invalid test cases for pattern clauses

--- src/unit_testing/src/TestQueryPreProcessor.cpp
+++ src/unit_testing/src/TestQueryPreProcessor.cpp
@@ -461,6 +461,29 @@ TEST_CASE (
     auto pat = Pattern::construct(syn, entRef, expr);
     REQUIRE(*(query->pattern) == pat);
   }
+  SECTION ("Pattern test UnderScore QuoteIdent") {
+    auto qp = QE::QueryPreprocessor();
+    std::string input =
+            "assign p;Select p such that Follows(a, b) pattern p (_, \"x\")";
+    auto query = qp.getQuery(input);
+    REQUIRE(*(query->declarations) ==
+            std::vector<Declaration>{Declaration(
+                    DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+    REQUIRE(
+            *(query->selected_declaration) ==
+            Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+    QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+    QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+    REQUIRE(*(query->such_that) ==
+            SuchThat::construct(Relation::Follows, a1, a2).value());
+
+    QE::Synonym syn = QE::Synonym::construct("p").value();
+    QE::EntRef entRef = QE::EntRef(QE::Underscore());
+    QE::ExpressionSpec expr = QE::QuoteIdent::construct("\"x\"").value()
+    auto pat = Pattern::construct(syn, entRef, expr);
+    REQUIRE(*(query->pattern) == pat);
+  }
   SECTION ("Pattern test QuoteIdent UnderScore") {
     auto qp = QE::QueryPreprocessor();
     std::string input =
@@ -530,6 +553,140 @@ TEST_CASE (
     auto pat = Pattern::construct(syn, entRef, expr);
     REQUIRE(*(query->pattern) == pat);
   }
+
+  SECTION ("Pattern test DoubleUnderscoreFactor UnderScore") {
+    auto qp = QE::QueryPreprocessor();
+    std::string input = "assign p;Select p such that Follows(a, b) pattern p (_\"y\"_, _)";
+    REQUIRE_THROWS_AS(qp.getQuery(input), QE::PQLParseException);
+  }
+}
+
+TEST_CASE (
+        "Test one assigned one select syntactically correct USES such that one "
+        "pattern") {
+    SECTION ("Pattern test QuoteIdent DoubleUnderscoreFactor") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input =
+                "assign p;Select p such that Uses(a, b) pattern p (\"x\", "
+                "_\"y\"_)";
+        auto query = qp.getQuery(input);
+        REQUIRE(*(query->declarations) ==
+                std::vector<Declaration>{Declaration(
+                        DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+        REQUIRE(
+                *(query->selected_declaration) ==
+                Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+        QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+        QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+        REQUIRE(*(query->such_that) ==
+                SuchThat::construct(Relation::UsesS, a1, a2).value());
+
+        QE::Synonym syn = QE::Synonym::construct("p").value();
+        QE::EntRef entRef = QE::EntRef(QE::QuoteIdent::construct("\"x\"").value());
+        QE::ExpressionSpec expr = QE::ExpressionSpec(
+                QE::DoubleUnderscoreFactor::construct("_\"y\"_").value());
+        auto pat = Pattern::construct(syn, entRef, expr);
+        REQUIRE(*(query->pattern) == pat);
+    }
+    SECTION ("Pattern test UnderScore QuoteIdent") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input =
+                "assign p;Select p such that Uses(a, b) pattern p (_, \"x\")";
+        auto query = qp.getQuery(input);
+        REQUIRE(*(query->declarations) ==
+                std::vector<Declaration>{Declaration(
+                        DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+        REQUIRE(
+                *(query->selected_declaration) ==
+                Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+        QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+        QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+        REQUIRE(*(query->such_that) ==
+                SuchThat::construct(Relation::UsesS, a1, a2).value());
+
+        QE::Synonym syn = QE::Synonym::construct("p").value();
+        QE::EntRef entRef = QE::EntRef(QE::Underscore());
+        QE::ExpressionSpec expr = QE::QuoteIdent::construct("\"x\"").value()
+        auto pat = Pattern::construct(syn, entRef, expr);
+        REQUIRE(*(query->pattern) == pat);
+    }
+    SECTION ("Pattern test QuoteIdent UnderScore") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input =
+                "assign p;Select p such that Uses(a, b) pattern p (\"x\", _)";
+        auto query = qp.getQuery(input);
+        REQUIRE(*(query->declarations) ==
+                std::vector<Declaration>{Declaration(
+                        DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+        REQUIRE(
+                *(query->selected_declaration) ==
+                Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+        QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+        QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+        REQUIRE(*(query->such_that) ==
+                SuchThat::construct(Relation::UsesS, a1, a2).value());
+
+        QE::Synonym syn = QE::Synonym::construct("p").value();
+        QE::EntRef entRef = QE::EntRef(QE::QuoteIdent::construct("\"x\"").value());
+        QE::ExpressionSpec expr = QE::ExpressionSpec(QE::Underscore());
+        auto pat = Pattern::construct(syn, entRef, expr);
+        REQUIRE(*(query->pattern) == pat);
+    }
+    SECTION ("Pattern test UnderScore UnderScore") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input =
+                "assign p;Select p such that Uses(a, b) pattern p (_, _)";
+        auto query = qp.getQuery(input);
+        REQUIRE(*(query->declarations) ==
+                std::vector<Declaration>{Declaration(
+                        DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+        REQUIRE(
+                *(query->selected_declaration) ==
+                Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+        QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+        QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+        REQUIRE(*(query->such_that) ==
+                SuchThat::construct(Relation::UsesS, a1, a2).value());
+
+        QE::Synonym syn = QE::Synonym::construct("p").value();
+        QE::EntRef entRef = QE::EntRef(QE::Underscore());
+        QE::ExpressionSpec expr = QE::ExpressionSpec(QE::Underscore());
+        auto pat = Pattern::construct(syn, entRef, expr);
+        REQUIRE(*(query->pattern) == pat);
+    }
+    SECTION ("Pattern test Synonym UnderScore") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input =
+                "assign p;Select p such that Uses(a, b) pattern p (x, _)";
+        auto query = qp.getQuery(input);
+        REQUIRE(*(query->declarations) ==
+                std::vector<Declaration>{Declaration(
+                        DesignEntity::ASSIGN, QE::Synonym::construct("p").value())});
+        REQUIRE(
+                *(query->selected_declaration) ==
+                Declaration(DesignEntity::ASSIGN, QE::Synonym::construct("p").value()));
+
+        QE::StmtOrEntRef a1 = QE::StmtRef(QE::Synonym::construct("a").value());
+        QE::StmtOrEntRef a2 = QE::StmtRef(QE::Synonym::construct("b").value());
+        REQUIRE(*(query->such_that) ==
+                SuchThat::construct(Relation::UsesS, a1, a2).value());
+
+        QE::Synonym syn = QE::Synonym::construct("p").value();
+        QE::EntRef entRef = QE::EntRef(QE::Synonym::construct("x").value());
+        QE::ExpressionSpec expr = QE::ExpressionSpec(QE::Underscore());
+        auto pat = Pattern::construct(syn, entRef, expr);
+        REQUIRE(*(query->pattern) == pat);
+    }
+
+    SECTION ("Pattern test DoubleUnderscoreFactor UnderScore") {
+        auto qp = QE::QueryPreprocessor();
+        std::string input = "assign p;Select p such that Follows(a, b) pattern p (_\"y\"_, _)";
+        REQUIRE_THROWS_AS(qp.getQuery(input), QE::PQLParseException);
+    }
 }
 
 // This should use REQUIRE_THROWS_MATCHES, but I cannot get the matcher to work
@@ -614,6 +771,18 @@ TEST_CASE ("Test Preprocess Exceptions") {
   SECTION ("Test malformed pattern - second argument") {
     auto qp = QE::QueryPreprocessor();
     std::string input = "assign p;Select p pattern a (_, @)";
+    REQUIRE_THROWS_AS(qp.getQuery(input), QE::PQLParseException);
+  }
+
+  SECTION ("Test mulitple such that query Preprocess") {
+    auto qp = QE::QueryPreprocessor();
+    std::string input = "assign p;Select p such that Follows(p , q) such that Modifies(p , q)";
+    REQUIRE_THROWS_AS(qp.getQuery(input), QE::PQLParseException);
+  }
+
+  SECTION ("Test mulitple pattern query Preprocess") {
+    auto qp = QE::QueryPreprocessor();
+    std::string input = "assign p;Select p pattern a (x, _) pattern b (y, _)";
     REQUIRE_THROWS_AS(qp.getQuery(input), QE::PQLParseException);
   }
 }

9b79770611e626fbcfcc2d41f166cd699bb56941
Sriram Sami <frizensami@gmail.com>
2019-03-14 12:37:41 +0800

    Add NextTEvaluator to QueryExecutor

---
+++ src/spa/src/query_executor/suchthat/NextTEvaluator.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <cassert>
+#include <optional>
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include "program_knowledge_base/pkb_manager.h"
+#include "query_builder/pql/pql.h"
+#include "query_executor/query_executor.h"
+#include "query_executor/suchthat/SuchThatEvaluator.h"
+
+using namespace PKB;
+using namespace QE;
+
+class NextTEvaluator : public SuchThatEvaluator {
+ public:
+  NextTEvaluator(Query* query, PKBManager* pkb, QueryConstraints& qc)
+      : SuchThatEvaluator(query, pkb, qc){};
+
+  std::unordered_set<std::string> handleLeftSynonymRightBasic(
+      std::string& basic_value) override {
+    // Next*(s, 3)
+    return pkb->getPreviousLineS(basic_value)
+        .value_or(std::unordered_set<std::string>());
+  }
+  std::unordered_set<std::string> handleRightSynonymLeftBasic(
+      std::string& basic_value) override {
+    // Next*(3, s)
+    return pkb->getNextLineS(basic_value)
+        .value_or(std::unordered_set<std::string>());
+  }
+  bool handleLeftSynonymRightUnderscore(std::string& arg_value) override {
+    // Next*(s, _) (for each s)
+    return pkb->getNextLineS(arg_value).has_value();
+  }
+  bool handleRightSynonymLeftUnderscore(std::string& arg_value) override {
+    // Next*(_, s) (for each s)
+    return pkb->getPreviousLineS(arg_value).has_value();
+  }
+  bool handleBothArgsSynonyms(std::string& arg_select,
+                              std::string& arg_unselect) override {
+    // Next*(s, s1)
+    return pkb->isLineNextLineS(arg_select, arg_unselect);
+  }
+  // Handle cases with no variables selected
+  bool handleDoubleUnderscore() override {
+    // TODO
+    // return !pkb->isLineFollowLineSSetEmpty();
+    assert(false);
+  }
+  bool handleLeftBasicRightUnderscore(std::string& arg) override {
+    // Next*(3, _)
+    return pkb->getNextLineS(arg).has_value();
+  }
+  bool handleRightBasicLeftUnderscore(std::string& arg) override {
+    // Next*(_, 3)
+    return pkb->getPreviousLineS(arg).has_value();
+  }
+  bool handleBothArgsBasic(std::string& arg1, std::string& arg2) override {
+    // Next*(2, 3)?
+    return pkb->isLineNextLineS(arg1, arg2);
+  }
+};

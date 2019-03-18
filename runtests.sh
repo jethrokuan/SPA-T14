c7beaabf0b49232e0f5045df2cb592d734b66902
Jethro Kuan <jethrokuan95@gmail.com>
2019-03-18 19:23:42 +0800

    Run iteration 2 tests

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Run Autotester on System Tests
 
 
@@ -113,10 +113,35 @@ build/src/autotester/autotester tests/system_tests/src/4.txt tests/system_tests/
 # RUN TESTS FOR SIMPLE SOURCE 5: MULTIPLE PROCEDURES // NOT REQUIRED FOR ITERATION 1
 
 # Tests without patterns
-# 1 Synonym, 0 Clause
-# build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5010.txt tests/system_tests/output/5010.xml
-# # 1 Synonym, 1 Clause (such that)
-# build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5011-st.txt tests/system_tests/output/5011-st.xml
-# # 2 Synonym, 1 Clause (such that)
-# build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5021-st.txt tests/system_tests/output/5021-st.xml
+1 Synonym, 0 Clause
+build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5010.txt tests/system_tests/output/5010.xml
+# 1 Synonym, 1 Clause (such that)
+build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5011-st.txt tests/system_tests/output/5011-st.xml
+# 2 Synonym, 1 Clause (such that)
+build/src/autotester/autotester tests/system_tests/src/5.txt tests/system_tests/query/5/5021-st.txt tests/system_tests/output/5021-st.xml
 
+
+# RUN BASIC TESTS (ITERATION TWO SPECIFIC QUERIES)
+build/src/autotester/autotester tests/system_tests/iteration-2/1-300-chain-of-calls.txt tests/system_tests/iteration-2/1-300-chain-of-calls-query.txt tests/system_tests/output/1.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/2-basic-call.txt tests/system_tests/iteration-2/2-basic-call-query.txt tests/system_tests/output/2-call.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/2-basic-call.txt tests/system_tests/iteration-2/2-basic-call-t-query.txt tests/system_tests/output/2-callt.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/3-basic-call-t.txt tests/system_tests/iteration-2/3-basic-call-t-query.txt tests/system_tests/output/3-callt.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/4-nest-while-next.txt tests/system_tests/iteration-2/4-nest-while-next-queries.txt tests/system_tests/output/4-next.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/4-nest-while-next.txt tests/system_tests/iteration-2/4-nest-while-next-t-queries.txt tests/system_tests/output/4-next-t.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/5-nest-if-next.txt tests/system_tests/iteration-2/5-nest-if-next-queries.txt tests/system_tests/output/5-next.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/5-nest-if-next.txt tests/system_tests/iteration-2/5-nest-if-next-t-queries.txt tests/system_tests/output/5-next-t.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/6-nest-if-balanced-next-queries.txt tests/system_tests/iteration-2/6-nest-if-balanced-next-queries.txt tests/system_tests/output/6-next.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/6-nest-if-balanced-next-queries.txt tests/system_tests/iteration-2/6-nest-if-balanced-next-t-queries.txt tests/system_tests/output/6-next-t.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/7-mixed-nest-next.txt tests/system_tests/iteration-2/7-mixed-nest-next-queries.txt tests/system_tests/output/7-next.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/7-mixed-nest-next.txt tests/system_tests/iteration-2/7-mixed-nest-next-t-queries.txt tests/system_tests/output/7-next-t.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/8-basic-uses-modifies.txt tests/system_tests/iteration-2/8-basic-uses-modifies-queries.txt tests/system_tests/output/8.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/9-basic-with-clause.txt tests/system_tests/iteration-2/9-basic-with-clause-queries.txt tests/system_tests/output/9.xml
+build/src/autotester/autotester tests/system_tests/iteration-2/10-basic-pattern.txt tests/system_tests/iteration-2/10-basic-pattern-queries.txt tests/system_tests/output/10.xml

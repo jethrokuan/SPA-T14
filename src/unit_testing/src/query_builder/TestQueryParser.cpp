606f5a40168a85e50f83cde561783ee43b22bce5
Jethro Kuan <jethrokuan95@gmail.com>
2019-03-21 17:22:03 +0800

    add tests for declaration

--- src/unit_testing/src/query_builder/TestQueryParser.cpp
+++ src/unit_testing/src/query_builder/TestQueryParser.cpp
@@ -7,10 +7,136 @@
 #include "query_builder/core/query_lexer.h"
 #include "query_builder/core/query_parser.h"
 #include "query_builder/pql/query.h"
+#include "query_builder/query_builder.h"
 
 using namespace QE;
 
-TEST_CASE ("Test Query Parser") {
+auto qe = QueryBuilder();
+
+TEST_CASE ("Test QueryParser Declarations") {
+  SECTION ("SUCCESS: Single Declarations") {
+    SECTION ("SUCCESS: assign a;Select a") {
+      std::string input = "assign a;Select a";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) ==
+              std::vector<Declaration>{
+                  Declaration(DesignEntity::ASSIGN, Synonym("a"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("a"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+
+    SECTION ("SUCCESS: variable v;Select v") {
+      std::string input = "variable v; Select v";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) ==
+              std::vector<Declaration>{
+                  Declaration(DesignEntity::VARIABLE, Synonym("v"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("v"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+
+    SECTION ("SUCCESS: stmt p;Select p") {
+      std::string input = "stmt p; Select p";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) == std::vector<Declaration>{Declaration(
+                                           DesignEntity::STMT, Synonym("p"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("p"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+
+    SECTION ("SUCCESS: read r;Select r") {
+      std::string input = "read r; Select r";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) == std::vector<Declaration>{Declaration(
+                                           DesignEntity::READ, Synonym("r"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("r"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+
+    SECTION ("SUCCESS: prog_line p;Select p") {
+      std::string input = "prog_line p; Select p";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) ==
+              std::vector<Declaration>{
+                  Declaration(DesignEntity::PROG_LINE, Synonym("p"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("p"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+  }
+
+  SECTION ("FAILURE: invalid design entities") {
+    SECTION ("FAILURE: Stmt p;Select p") {
+      std::string input = "Stmt p; Select p";
+      REQUIRE_THROWS_WITH(qe.makePqlQuery(input),
+                          "Expected 'Select', got 'Stmt'.");
+    }
+
+    SECTION ("FAILURE: progline p;Select p") {
+      std::string input = "progline p; Select p";
+      REQUIRE_THROWS_WITH(qe.makePqlQuery(input),
+                          "Expected 'Select', got 'progline'.");
+    }
+  }
+
+  SECTION ("SUCCESS: multiple declarations") {
+    SECTION ("SUCCESS: assign a1, a2; Select a2") {
+      std::string input = "assign a1, a2; Select a2";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) ==
+              std::vector<Declaration>{
+                  Declaration(DesignEntity::ASSIGN, Synonym("a1")),
+                  Declaration(DesignEntity::ASSIGN, Synonym("a2"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("a2"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+
+    SECTION ("SUCCESS: assign a1; stmt s1; Select a1") {
+      std::string input = "assign a1; stmt s1; Select a1";
+      auto query = qe.makePqlQuery(input);
+      REQUIRE(*(query.declarations) ==
+              std::vector<Declaration>{
+                  Declaration(DesignEntity::ASSIGN, Synonym("a1")),
+                  Declaration(DesignEntity::STMT, Synonym("s1"))});
+      REQUIRE(query.result->selected_declarations->size() == 1);
+      REQUIRE(std::get<Synonym>(query.result->selected_declarations->at(0)) ==
+              Synonym("a1"));
+      REQUIRE(query.rel_cond->empty());
+      REQUIRE(query.patternb->empty());
+      REQUIRE(query.with_cond->empty());
+    }
+  }
+
+  SECTION ("FAILURE: Syntax errors") {
+    SECTION ("SUCCESS: assign a1, stmt s1; Select a1") {
+      std::string input = "assign a1, stmt s1; Select a1";
+      REQUIRE_THROWS_WITH(qe.makePqlQuery(input), "Expected, ',', got 's1'.");
+    }
+  }
+
   SECTION ("Test one assign one select query") {
     std::string input =
         "assign a, a1; procedure p; Select <a, p> such that Modifies (a, "

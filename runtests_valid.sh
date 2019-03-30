2d777a54a07b766167781d73695cf802d37ddc50
aspiringdevslog <aspiringdevslog@gmail.com>
2019-03-30 16:53:24 +0800

    Revert "Revert "fix shell for iteration 1 test""
    This reverts commit 046e9043123220225e5c4e7401f0a1e756406425.

--- runtests_valid.sh
+++ runtests_valid.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 # Run Autotester on System Tests
 
-
 # CREATE THE OUTPUT DIRECTORY
 mkdir -p tests/system_tests/output
 
 # ITERATION 1
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1010.txt tests/system_tests/output/iteration1-1010.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1011-p.txt tests/system_tests/output/iteration1-1011-p.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1011-st.txt tests/system_tests/output/iteration1-1011-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1012.txt tests/system_tests/output/iteration1-1012.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1020.txt tests/system_tests/output/iteration1-1020.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1021-p.txt tests/system_tests/output/iteration1-1021-p.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1021-st.txt tests/system_tests/output/iteration1-1021-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1022.txt tests/system_tests/output/iteration1-1022.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/1.txt /tests/system_tests/iteration-1/1022-new.txt tests/system_tests/output/iteration1-1022-new.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2010.txt tests/system_tests/output/iteration1-2010.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2011-p.txt tests/system_tests/output/iteration1-2011-p.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2011-st.txt tests/system_tests/output/iteration1-2011-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2012.txt tests/system_tests/output/iteration1-2012.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2020.txt tests/system_tests/output/iteration1-2020.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2021-p.txt tests/system_tests/output/iteration1-2021-p.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2021-st.txt tests/system_tests/output/iteration1-2021-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/2.txt /tests/system_tests/iteration-1/2022.txt tests/system_tests/output/iteration1-2022.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/4.txt /tests/system_tests/iteration-1/4010.txt tests/system_tests/output/iteration1-4010.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/4.txt /tests/system_tests/iteration-1/4011-st.txt tests/system_tests/output/iteration1-4011-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/4.txt /tests/system_tests/iteration-1/4021-st.txt tests/system_tests/output/iteration1-4021-st.xml
-build/src/autotester/autotester tests/system_tests/iteration-1/4.txt /tests/system_tests/iteration-1/4022.txt tests/system_tests/output/iteration1-4022.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1010.txt tests/system_tests/output/iteration1-1010.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1011-p.txt tests/system_tests/output/iteration1-1011-p.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1011-st.txt tests/system_tests/output/iteration1-1011-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1012.txt tests/system_tests/output/iteration1-1012.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1020.txt tests/system_tests/output/iteration1-1020.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1021-p.txt tests/system_tests/output/iteration1-1021-p.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1021-st.txt tests/system_tests/output/iteration1-1021-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1022.txt tests/system_tests/output/iteration1-1022.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/1.txt tests/system_tests/iteration-1/1022-new.txt tests/system_tests/output/iteration1-1022-new.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2010.txt tests/system_tests/output/iteration1-2010.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2011-p.txt tests/system_tests/output/iteration1-2011-p.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2011-st.txt tests/system_tests/output/iteration1-2011-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2012.txt tests/system_tests/output/iteration1-2012.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2020.txt tests/system_tests/output/iteration1-2020.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2021-p.txt tests/system_tests/output/iteration1-2021-p.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2021-st.txt tests/system_tests/output/iteration1-2021-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/2.txt tests/system_tests/iteration-1/2022.txt tests/system_tests/output/iteration1-2022.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-1/4.txt tests/system_tests/iteration-1/4010.txt tests/system_tests/output/iteration1-4010.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/4.txt tests/system_tests/iteration-1/4011-st.txt tests/system_tests/output/iteration1-4011-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/4.txt tests/system_tests/iteration-1/4021-st.txt tests/system_tests/output/iteration1-4021-st.xml
+build/src/autotester/autotester tests/system_tests/iteration-1/4.txt tests/system_tests/iteration-1/4022.txt tests/system_tests/output/iteration1-4022.xml
 
 # ITERATION 2 - SET 0 
 build/src/autotester/autotester tests/system_tests/iteration-2/set0/1-300-chain-of-calls.txt tests/system_tests/iteration-2/set0/1-300-chain-of-calls-query.txt tests/system_tests/output/iteration2-1-300-chain-of-calls-query.xml
@@ -44,7 +45,6 @@ build/src/autotester/autotester tests/system_tests/iteration-2/set0/7-mixed-nest
 build/src/autotester/autotester tests/system_tests/iteration-2/set0/8-basic-uses-modifies.txt tests/system_tests/iteration-2/set0/8-basic-uses-modifies-queries.txt tests/system_tests/output/iteration2-8-basic-uses-modifies-queries.xml
 build/src/autotester/autotester tests/system_tests/iteration-2/set0/9-basic-with-clause.txt tests/system_tests/iteration-2/set0/9-basic-with-clause-queries.txt tests/system_tests/output/iteration2-9-basic-with-clause-queries.xml
 build/src/autotester/autotester tests/system_tests/iteration-2/set0/10-basic-pattern.txt tests/system_tests/iteration-2/set0/10-basic-pattern-queries.txt tests/system_tests/output/iteration2-10-basic-pattern-queries.xml
-
 
 # ITERATION 2 - SET 1
 build/src/autotester/autotester tests/system_tests/iteration-2/set1/source-1.txt tests/system_tests/iteration-2/set1/1-calls.txt tests/system_tests/output/iteration2-1-calls.xml

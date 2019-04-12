42103dcc9bc046bb676bec5128203938ec85faaf
Jethro Kuan <jethrokuan95@gmail.com>
2019-04-12 23:41:51 +0800

    Add iter3 tests to run-all-tests.sh

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -62,7 +62,36 @@ build/src/autotester/autotester tests/system_tests/iteration-2/set0/10-basic-pat
 # build/src/autotester/autotester tests/system_tests/iteration-2/set1/source-1.txt tests/system_tests/iteration-2/set1/1-pattern-container.txt tests/system_tests/output/iteration2-1-pattern-container.xml
 # build/src/autotester/autotester tests/system_tests/iteration-2/set1/source-1.txt tests/system_tests/iteration-2/set1/1-uses.txt tests/system_tests/output/iteration2-1-uses.xml
 build/src/autotester/autotester tests/system_tests/iteration-2/set1/source-1.txt tests/system_tests/iteration-2/set1/1-collated-basic-test.txt tests/system_tests/output/iteration2-1-collated-basic-test.xml
-
 build/src/autotester/autotester tests/system_tests/iteration-2/11-more-with-clause-source.txt tests/system_tests/iteration-2/11-more-with-clause-queries.txt tests/system_tests/output/iteration2-11-more-with-clause.xml
 
-# build/src/autotester/autotester tests/system_tests/iteration-2/set1/source-1.txt tests/system_tests/iteration-2/set1/temp-failed-queries.txt tests/system_tests/output/temp-failed-queries.xml
+# INVALIDS
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/1-invalid-source.txt tests/system_tests/iteration-2/invalids/1-invalid-calls-query.txt tests/system_tests/output/1-invalid-calls-query.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/1-invalid-source.txt tests/system_tests/iteration-2/invalids/1-invalid-calls-t-query.txt tests/system_tests/output/1-invalid-calls-t-query.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/1-invalid-source.txt tests/system_tests/iteration-2/invalids/1-invalid-next-query.txt tests/system_tests/output/1-invalid-next-query.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/1-invalid-source.txt tests/system_tests/iteration-2/invalids/1-invalid-next-t-query.txt tests/system_tests/output/1-invalid-next-t-query.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/2-invalid-calls-source.txt tests/system_tests/iteration-2/invalids/2-invalid-calls-query.txt tests/system_tests/output/2-invalid-calls-query.txt.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/3-invalid-calls-t-source.txt tests/system_tests/iteration-2/invalids/3-invalid-calls-t-query.txt tests/system_tests/output/3-invalid-calls-t-query.txt.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-2/invalids/4-invalid-uses-source.txt tests/system_tests/iteration-2/invalids/4-invalid-uses-query.txt tests/system_tests/output/4-invalid-uses-query.txt.xml
+
+# ITERATION 3
+# AFFECTS/AFFECTS*
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/1-source.txt tests/system_tests/iteration-3/affects/1-affects.txt tests/system_tests/output/iter3-1-affects.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/2-source.txt tests/system_tests/iteration-3/affects/2-affects.txt tests/system_tests/output/iter3-2-affects.xml
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/2-source.txt tests/system_tests/iteration-3/affects/2-affects-t.txt tests/system_tests/output/iter3-2-affects-t.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/3-source.txt tests/system_tests/iteration-3/affects/3-affects-both.txt tests/system_tests/output/iter3-3-affects-both.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/4-source.txt tests/system_tests/iteration-3/affects/4-affects-double-clause.txt tests/system_tests/output/iter3-4-affects-double-clause.xml
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/4-source.txt tests/system_tests/iteration-3/affects/4-affects-t-double-clause.txt tests/system_tests/output/iter3-4-affects-t-double-clause.xml
+
+build/src/autotester/autotester tests/system_tests/iteration-3/affects/5-source.txt tests/system_tests/iteration-3/affects/5-multi-clause.txt tests/system_tests/output/iter3-5-multi.xml
+
+#  STRESS TEST
+# build/src/autotester/autotester tests/system_tests/iteration-3/stress-test/1-source-read-assign.txt tests/system_tests/iteration-3/stress-test/1-query.txt tests/system_tests/output/iter3-stress-test-01.xml

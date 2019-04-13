3e8533a5a047b52878ccddc3cfe37547e4468b64
Jethro Kuan <jethrokuan95@gmail.com>
2019-04-13 17:20:02 +0800

    trim QuoteIdents
    because apparently it's in the spec

--- src/spa/src/utils/utils.h
+++ src/spa/src/utils/utils.h
@@ -1,6 +1,7 @@
 #pragma once
-
 #include <algorithm>
+#include <cctype>
+#include <locale>
 #include <iostream>
 #include <iterator>
 #include <sstream>
@@ -138,9 +139,43 @@ std::unordered_map<T, std::vector<T>> getMapFromPairSet(
 bool has_only_digits(const std::string);
 bool is_valid_name(const std::string);
 
-std::string& ltrim(std::string&, const std::string& chars = "\t ");
-std::string& rtrim(std::string&, const std::string& chars = "\t ");
-std::string& trim(std::string&, const std::string& chars = "\t ");
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
+    return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+}
 
 // Utility templates for std::visit
 template <class... Ts>
